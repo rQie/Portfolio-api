66a7f7846ecce8c5f626934aecde83affe9da0df
rQie <rifqieakma@gmail.com>
2020-08-17 14:26:55 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0087dbb902746e49000a623d233d1425'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0200ac1d-41cc-4045-bad5-464d70e67117'
+};
